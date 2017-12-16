f82da2ff7af90dc20d2036e3e8ff687543a41ffc
Jim McAndrew <jim@loc8.us>
2017-12-15 23:31:48 -0500

    Add testing and run precompile

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -16,9 +16,8 @@ echo "website creating"
 # The DB should already be created at this point
 # docker-compose exec website bundle exec rake db:create
 echo "website pre-compile images"
-docker-compose exec website rails RAILS_ENV=production rake assets:precompile
+docker-compose exec website bash RAILS_ENV=production rake assets:precompile
 echo "website migrating"
 docker-compose exec website bundle exec rake db:migrate
-# echo "website testing"
-# docker-compose exec website bundle exec rake test:db
-
+echo "website testing"
+docker-compose exec website bundle exec rake test:db
