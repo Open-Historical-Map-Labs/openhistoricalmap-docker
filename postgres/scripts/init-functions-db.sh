3b1c140592f701617c032d6facee9f92f6c58104
Jim McAndrew <jim@loc8.us>
2017-12-23 01:26:21 -0500

    Add postgis and osm2pgsql support

--- postgres/scripts/init-functions-db.sh
+++ postgres/scripts/init-functions-db.sh
@@ -7,6 +7,8 @@ if [ "`whoami`" == "root" ]; then
     libpq-dev \
     postgresql-contrib-9.6 \
     postgresql-server-dev-9.6 \
+    postgresql-9.6-postgis-2.4 \
+    osm2pgsql \
      && rm -rf /var/lib/apt/lists/*
   cd /openstreetmap-website/db/functions
   make libpgosm.so
