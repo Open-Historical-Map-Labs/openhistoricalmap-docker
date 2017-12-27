a93e3dcf0ca3fb64ea203e01c9ed36e8b92b0941
Jim McAndrew <jim@loc8.us>
2017-12-27 15:11:31 -0500

    remove the osmosis and osm2pgsql installs from the api server

--- postgres/scripts/init-functions-db.sh
+++ postgres/scripts/init-functions-db.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 if [ "`whoami`" == "root" ]; then
+
+  # Add postgresql contribs in order to build libpgosm.so
   apt-get update  -qq && \
-    apt-get install -y --no-install-recommends \
-    build-essential \
-    libpq-dev \
-    postgresql-contrib-9.6 \
-    postgresql-server-dev-9.6 \
-    postgresql-9.6-postgis-2.4 \
-    osm2pgsql \
-    curl \
-     && rm -rf /var/lib/apt/lists/*
+      apt-get install -y --no-install-recommends \
+      build-essential \
+      libpq-dev \
+      postgresql-contrib-9.6 \
+      postgresql-server-dev-9.6 \
+       && rm -rf /var/lib/apt/lists/*
+
   cd /openstreetmap-website/db/functions
   make libpgosm.so
+
 fi
