456dc73c91b977ec45e712e8637e14bf1d7f99f3
Jim McAndrew <jim@loc8.us>
2017-12-27 15:17:20 -0500

    Add code to register iD

--- scripts/register_id.sh
+++ scripts/register_id.sh
@@ -1,2 +1,2 @@
-cmd="psql -U postgres osm -c \"INSERT INTO client_applications values('1','iD','http://localhost:3000',null,'http://localhost:3000','bzUV0OjdHUbvnABc310DF2MsDF1HLwZGdb6yhMCv','czw8tPg0Wtfc0Pfpa0JO5Esk29TP2o7OTaUJjB8q',1,now(),now(),'t','t','t','t','t','t','t');\""
+cmd="psql -U postgres osm -c \"INSERT INTO client_applications values('1','iD','http://localhost:3000',null,'http://localhost:3000','bzUV0OjdHUbvnABc310DF2MsDF1HLwZGdb6yhMCv','czw8tPg0Wtfc0Pfpa0JO5Esk29TP2o7OTaUJjB8q',0,now(),now(),'t','t','t','t','t','t','t');\""
 docker-compose exec postgres /bin/bash -c "$cmd"
