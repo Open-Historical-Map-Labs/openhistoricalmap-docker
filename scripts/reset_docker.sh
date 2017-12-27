9c93d3034ec55ee8bef65cdcc8ec5f6ea2bc65ac
Jim McAndrew <jim@loc8.us>
2017-12-27 15:17:37 -0500

    Add code to reset everything (for testing only)

--- scripts/reset_docker.sh
+++ scripts/reset_docker.sh
@@ -1,2 +1,3 @@
 sudo docker rmi $(sudo docker images -aq) -f && sudo docker rm $(sudo docker ps -aq) -f
 sudo rm -rf ./postgres/data && mkdir -p ./postgres/data
+sudo rm -rf ./rendering_database/data && mkdir -p ./rendering_database/data
