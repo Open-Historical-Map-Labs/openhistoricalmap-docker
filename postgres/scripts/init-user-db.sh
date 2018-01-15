a2669f5d929cb278ca67aad5de7cbb5b7f94b19c
Jimmy Rocks <jim@loc8.us>
2018-01-15 22:26:51 +0000

    Give the POSTGISDB_NAME variable a more descriptove name

--- postgres/scripts/init-user-db.sh
+++ postgres/scripts/init-user-db.sh
@@ -5,26 +5,26 @@ psql -U postgres -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWOR
 
 set -e
 
-until psql -U postgres -d $APIDB_NAME -c "CREATE EXTENSION IF NOT EXISTS btree_gist"
+until psql -U postgres -d $POSTGISDB_NAME -c "CREATE EXTENSION IF NOT EXISTS btree_gist"
 do
-    echo "Waiting for postgres $APIDB_NAME database ready..."
+    echo "Waiting for postgres $POSTGISDB_NAME database ready..."
     sleep 2
 done
 
-until psql -U postgres -d $APIDB_NAME -c "CREATE FUNCTION maptile_for_point(int8, int8, int4) RETURNS int4 AS '/openstreetmap-website/db/functions/libpgosm', 'maptile_for_point' LANGUAGE C STRICT"
+until psql -U postgres -d $POSTGISDB_NAME -c "CREATE FUNCTION maptile_for_point(int8, int8, int4) RETURNS int4 AS '/openstreetmap-website/db/functions/libpgosm', 'maptile_for_point' LANGUAGE C STRICT"
 do
-    echo "Waiting for postgres $APIDB_NAME btree_gist..."
+    echo "Waiting for postgres $POSTGISDB_NAME btree_gist..."
     sleep 2
 done
 
-until psql -U postgres -d $APIDB_NAME -c "CREATE FUNCTION tile_for_point(int4, int4) RETURNS int8 AS '/openstreetmap-website/db/functions/libpgosm', 'tile_for_point' LANGUAGE C STRICT"
+until psql -U postgres -d $POSTGISDB_NAME -c "CREATE FUNCTION tile_for_point(int4, int4) RETURNS int8 AS '/openstreetmap-website/db/functions/libpgosm', 'tile_for_point' LANGUAGE C STRICT"
 do
     echo "Waiting for postgres maptile_for_point..."
     sleep 2
 done
 
-until psql -U postgres -d $APIDB_NAME -c "CREATE FUNCTION xid_to_int4(xid) RETURNS int4 AS '/openstreetmap-website/db/functions/libpgosm', 'xid_to_int4' LANGUAGE C STRICT"
+until psql -U postgres -d $POSTGISDB_NAME -c "CREATE FUNCTION xid_to_int4(xid) RETURNS int4 AS '/openstreetmap-website/db/functions/libpgosm', 'xid_to_int4' LANGUAGE C STRICT"
 do
-    echo "Waiting for postgres $APIDB_NAME tile_for_point..."
+    echo "Waiting for postgres $POSTGISDB_NAME tile_for_point..."
     sleep 2
 done
