92fce53b1cb6682b1ac85faa3c922fa07a828c6f
Jimmy Rocks <jim@loc8.us>
2018-01-15 21:11:25 +0000

    Don't run tests when in prod

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -10,7 +10,7 @@ docker-compose up -d
 # Create the database
 echo "pg functions"
 docker-compose exec postgres /bin/bash /docker-entrypoint-initdb.d/init-functions-db.sh
-echo "pg user info (build in the functions)"
+echo "pg user info (associate the functions to the database)"
 docker-compose exec postgres /bin/bash /docker-entrypoint-initdb.d/init-user-db.sh
 echo "website env"
 docker-compose exec website rails db:environment:set RAILS_ENV=production
@@ -22,8 +22,9 @@ echo "set up iD"
 docker-compose exec postgres /bin/bash /docker-entrypoint-initdb.d/init-id.sh
 echo "set up rendering database"
 docker-compose exec rendering_database /bin/bash /docker-entrypoint-initdb.d/init-rendering-db.sh
-echo "website testing"
-docker-compose exec website bundle exec rake test:db
+# Disable testing in the production environment (can be destructive to data)
+# echo "website testing"
+# docker-compose exec website bundle exec rake test:db
 
 echo "Restart the website"
 docker-compose stop website && docker-compose start website
