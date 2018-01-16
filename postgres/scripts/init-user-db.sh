3fd2a6684e5a08625c620f1796491a8f74788e5a
Jim McAndrew <jim@loc8.us>
2018-01-15 23:59:21 -0500

    Use the POSTGRES_DATABASE env variable

--- postgres/scripts/init-user-db.sh
+++ postgres/scripts/init-user-db.sh
@@ -5,26 +5,26 @@ psql -U postgres -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWOR
 
 set -e
 
-until psql -U postgres -d $POSTGISDB_NAME -c "CREATE EXTENSION IF NOT EXISTS btree_gist"
+until psql -U postgres -d $POSTGRES_DATABASE -c "CREATE EXTENSION IF NOT EXISTS btree_gist"
 do
-    echo "Waiting for postgres $POSTGISDB_NAME database ready..."
+    echo "Waiting for postgres $POSTGRES_DATABASE database ready..."
     sleep 2
 done
 
-until psql -U postgres -d $POSTGISDB_NAME -c "CREATE OR REPLACE FUNCTION maptile_for_point(int8, int8, int4) RETURNS int4 AS '/openstreetmap-website/db/functions/libpgosm', 'maptile_for_point' LANGUAGE C STRICT"
+until psql -U postgres -d $POSTGRES_DATABASE -c "CREATE OR REPLACE FUNCTION maptile_for_point(int8, int8, int4) RETURNS int4 AS '/openstreetmap-website/db/functions/libpgosm', 'maptile_for_point' LANGUAGE C STRICT"
 do
-    echo "Waiting for postgres $POSTGISDB_NAME btree_gist..."
+    echo "Waiting for postgres btree_gist..."
     sleep 2
 done
 
-until psql -U postgres -d $POSTGISDB_NAME -c "CREATE OR REPLACE FUNCTION tile_for_point(int4, int4) RETURNS int8 AS '/openstreetmap-website/db/functions/libpgosm', 'tile_for_point' LANGUAGE C STRICT"
+until psql -U postgres -d $POSTGRES_DATABASE -c "CREATE OR REPLACE FUNCTION tile_for_point(int4, int4) RETURNS int8 AS '/openstreetmap-website/db/functions/libpgosm', 'tile_for_point' LANGUAGE C STRICT"
 do
     echo "Waiting for postgres maptile_for_point..."
     sleep 2
 done
 
-until psql -U postgres -d $POSTGISDB_NAME -c "CREATE OR REPLACE FUNCTION xid_to_int4(xid) RETURNS int4 AS '/openstreetmap-website/db/functions/libpgosm', 'xid_to_int4' LANGUAGE C STRICT"
+until psql -U postgres -d $POSTGRES_DATABASE -c "CREATE OR REPLACE FUNCTION xid_to_int4(xid) RETURNS int4 AS '/openstreetmap-website/db/functions/libpgosm', 'xid_to_int4' LANGUAGE C STRICT"
 do
-    echo "Waiting for postgres $POSTGISDB_NAME tile_for_point..."
+    echo "Waiting for postgres xid_to_int4..."
     sleep 2
 done
