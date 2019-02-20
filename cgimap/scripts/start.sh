2ed64322ba4c61de2e26f85f8b796d6f5e0e1651
Seth Fitzsimmons <seth@mojodna.net>
2019-02-20 13:29:50 -0800

    Clean up CGImap startup

--- cgimap/scripts/start.sh
+++ cgimap/scripts/start.sh
@@ -1,5 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
-envsubst < /lighttpd.env.conf > /lighttpd.conf
-lighttpd -f /lighttpd.conf &
-/bin/bash
+envsubst '$POSTGRES_HOST $POSTGRES_DATABASE $POSTGRES_USER $POSTGRES_PASSWORD' < /lighttpd.env.conf > /lighttpd.conf
+lighttpd -f /lighttpd.conf -D
