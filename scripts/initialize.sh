4ed07402f3b37c5dadacfeb461ad72a0ff41a219
Seth Fitzsimmons <seth@mojodna.net>
2019-02-20 13:01:11 -0800

    Match split Docker Compose configs

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,21 +1,18 @@
-# Move the configs
-sudo cp ./website/config/* ./website/openstreetmap-website/config/
-
 # Build the dockers
 echo "Building Docker"
-docker-compose build
+docker-compose -f docker-compose.yml -f docker-compose.postgresql.yml build
 echo "Bring Docker Up"
-docker-compose up -d
+docker-compose -f docker-compose.yml -f docker-compose.postgresql.yml up -d
 
 # Create the database
 echo "pg functions: create the database"
-docker-compose exec postgres /bin/bash /docker-entrypoint-initdb.d/init-functions-db.sh
+docker-compose -f docker-compose.yml -f docker-compose.postgresql.yml exec postgres /bin/bash /docker-entrypoint-initdb.d/init-functions-db.sh
 echo "add pg user info and associate the functions to the database"
-docker-compose exec postgres /bin/bash /docker-entrypoint-initdb.d/init-user-db.sh
+docker-compose -f docker-compose.yml -f docker-compose.postgresql.yml exec postgres /bin/bash /docker-entrypoint-initdb.d/init-user-db.sh
 echo "website: migrating database"
 docker-compose exec website bundle exec rake db:migrate
 echo "add the key to iD"
-docker-compose exec postgres /bin/bash /docker-entrypoint-initdb.d/init-id.sh
+docker-compose -f docker-compose.yml -f docker-compose.postgresql.yml exec postgres /bin/bash /docker-entrypoint-initdb.d/init-id.sh
 # Disable testing in the production environment (can be destructive to data)
 # echo "website testing"
 # docker-compose exec website bundle exec rake test:db
