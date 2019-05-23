2d77f49d355beaa28c2f23bd88f27db55da34710
Jim McAndrew <jim@loc8.us>
2019-05-22 22:27:53 -0400

    Update the function to add the SQL versions of these functions

--- postgres/scripts/init-psql-functions.sh
+++ postgres/scripts/init-psql-functions.sh
@@ -7,3 +7,17 @@ do
     echo "Waiting for postgres $POSTGRES_DATABASE database ready..."
     sleep 2
 done
+
+until psql $DATABASE_URL -f /docker-entrypoint-initdb.d/maptile_for_point.sql
+do
+    echo "Waiting for postgres $POSTGRES_DATABASE database ready..."
+    sleep 2
+done
+
+until psql $DATABASE_URL -f /docker-entrypoint-initdb.d/tile_for_point.sql
+do
+    echo "Waiting for postgres $POSTGRES_DATABASE database ready..."
+    sleep 2
+done
+
+
